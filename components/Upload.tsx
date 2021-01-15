e36f936226504411e6b88fdc21ac9311dee999f5
fujiokayu <fujiokayu@gmail.com>
2021-01-15 22:21:43 +0900

    change file upload to using base64 string

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,30 +1,29 @@
 const Upload = (props) => {
-  function onSubmit(event) {
+
+  async function onSubmit(event) {
     event.preventDefault()
 
-    const file: File = props.file.file
-    let reader = new FileReader;
-    reader.readAsArrayBuffer(file);
-  
-    reader.onload = () => {
-      console.log(reader.result)
-      fetch("/api/upload", {
-        method: 'POST',
-        headers: new Headers({ 
-          'Content-Type': 'application/octet-binary', 
-          'token': props.user.token, 
-          'name': props.file.name}),
-        credentials: 'same-origin',
-        body: reader.result
-        // Todo: API resolved without sending a response for /api/upload, this may result in stalled requests.
-      }).then((res) => res.json())      
-      .then(data => {
-        console.log(data);
-      })
-      .catch(error => {
-        console.error(error);
-      })
-    }
+    const blob: File = props.file.file
+    const arrayBuffer = await blob.arrayBuffer();
+    const base64String = btoa(String.fromCharCode(...new Uint8Array(arrayBuffer)));
+
+    console.log('base64String: ', base64String)
+    fetch("/api/upload", {
+      method: 'POST',
+      headers: new Headers({ 
+        //'Content-Type': 'application/octet-stream', 
+        'token': props.user.token, 
+        'name': props.file.name}),
+      credentials: 'same-origin',
+      body: base64String
+      // Todo: API resolved without sending a response for /api/upload, this may result in stalled requests.
+    }).then((res) => res.json())      
+    .then(data => {
+      console.log('fetch result: ', data);
+    })
+    .catch(error => {
+      console.error(error);
+    })
   }
   return (
     <form onSubmit={onSubmit}>
--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,16 +1,17 @@
 import { verifyIdToken } from '../../utils/auth/firebaseAdmin'
 import { uploadFile } from '../../utils/storage/uploadFile'
+import { Buffer } from 'buffer';
 
 const upload = async (req, res) => {
   const token = req.headers.token
   const name = req.headers.name
-  const source = req.body
+  const encodedSource: string = req.body
 
   try {
     const verifideToken = await verifyIdToken(token)
     const path = verifideToken.uid + '/' + name
 
-    await uploadFile(path, source)
+    await uploadFile(path, encodedSource)
     return res.status(200)
   } catch (error) {
     console.log(error)
