50940b7d285a1c45786bb3c5357a911e5dbdff9d
fujiokayu <fujiokayu@gmail.com>
2021-01-24 21:05:19 +0900

    modify download functions to handle admin claims

--- pages/api/download.ts
+++ pages/api/download.ts
@@ -1,15 +1,23 @@
 import { verifyIdToken } from '../../utils/auth/firebaseAdmin'
 import { downloadFile } from '../../utils/storage/downloadFile'
+import * as admin from 'firebase-admin'
 
 const download = async (req, res) => {
   const token: string = req.headers.token
   const fileName: string = req.headers.filename
 
   try {
-    const verifideToken = await verifyIdToken(token)
-    const path: string = verifideToken.uid + '/' + fileName
+    const verifiedToken = await verifyIdToken(token)
+    const user = await admin.auth().getUser(verifiedToken.uid)
 
-    const data = await downloadFile(path)
+    const parent = fileName.replace(/\/[^\/]+$/, '')
+
+    // customClaims.admin、もしくは uid がフォルダ名と一致した時はダウンロードを許可する
+    if (!(user.customClaims && user.customClaims.admin === true || parent === verifiedToken.uid)) {
+      return res.status(401).send('You are unauthorised')
+    }
+
+    const data = await downloadFile(fileName)
     const base64String = data.toString()
 
     console.log(base64String.length)
