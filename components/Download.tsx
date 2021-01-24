c42b5b356b99e7415c10a3e0bb5b9729822bfd32
fujiokayu <fujiokayu@gmail.com>
2021-01-24 21:04:46 +0900

    change props of Download component from file name to File object

--- components/Download.tsx
+++ components/Download.tsx
@@ -3,12 +3,11 @@ import fileDownload from 'js-file-download'
 const Download = (props) => {
   async function onSubmit(event) {
     event.preventDefault()
-
     const response = await fetch("/api/download", {
       method: 'GET',
       headers: new Headers({ 
         'token': props.token, 
-        'fileName': props.file}),
+        'fileName': props.file.name}),
       credentials: 'same-origin',
       // Todo: API resolved without sending a response for /api/upload, this may result in stalled requests.
     })
@@ -23,7 +22,7 @@ const Download = (props) => {
     }
     const blob = new Blob([array], {type: 'application/octet-stream'});
 
-    fileDownload(blob, props.file);
+    fileDownload(blob, props.file.name.substring(props.file.name.indexOf('/')+1));
   }
 
   return (
--- components/List.tsx
+++ components/List.tsx
@@ -26,7 +26,7 @@ const List = (props) => {
               <li key={file.metadata.id.toString()}>
                 {file.name.substring(file.name.indexOf('/')+1)}: {file.metadata.contentType} {Math.round(file.metadata.size / 1024 / 4 * 3)}kb
                 last updated: {format(new Date(file.metadata.updated), 'yyyy/MM/dd HH:mm:ss')}
-                <Download token={props.user.token} file={file.name.substring(file.name.indexOf('/')+1)}/>
+                <Download token={props.user.token} file={file}/>
                 <hr />
               </li>
             ))
