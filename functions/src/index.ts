076d13475e9b203afcc437591b66bcef795ba57c
fujiokayu <fujiokayu@gmail.com>
2021-02-15 20:57:52 +0900

    disable uninvited user sign up

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -14,20 +14,36 @@ admin.initializeApp({
   databaseURL: 'https://lobster-97151.firebaseio.com',
 })
 const adminRef = admin.firestore().collection('administrators')
+const invitedRef = admin.firestore().collection('invited')
+const userRef = admin.firestore().collection('user')
 
 exports.setCustomClaim = functions
   .region('asia-northeast1')
   .auth.user()
   .onCreate(async (user) => {
-    const querySnapshot = await adminRef.where('email', '==', user.email).get()
-    if (querySnapshot.size > 0) {
-      console.log('set admin claim with: ', user.email )
-      await admin.auth().setCustomUserClaims(user.uid, { admin: true })
-      return
+    const adminQuerySnapshot = await adminRef.where('email', '==', user.email).get()
+    const invitedQuerySnapshot = await invitedRef.where('email', '==', user.email).get()
+
+    // いずれのデータベースにも登録されていないユーザーのサインアップは無効化する
+    if ( adminQuerySnapshot.size === 0 && invitedQuerySnapshot.size === 0) {
+      return admin.auth()
+      .updateUser(user.uid, {disabled: true})
+      .then(userRecord => console.log(`Auto blocked user: ${userRecord.toJSON()}`))
+      .catch(error => console.log(`Error auto blocking: ${error}`))
     }
 
-    console.log('create user: ', user.email)
-    return
+    if (adminQuerySnapshot.size > 0) {
+      console.log('set admin claim with: ', user.email )
+      await admin.auth().setCustomUserClaims(user.uid, { admin: true })
+    }
+
+    if (invitedQuerySnapshot.size > 0) {
+      console.log('create user: ', user.email )
+      await userRef.add({
+        id: user.uid,
+        email: user.email,
+      })
+    }
   })
 /*
 exports.onDeleteUser = functions.auth.user().onDelete((user) => {
