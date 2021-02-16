1ac1869e5d17428c28f592238a2fba854ef955e4
fujiokayu <fujiokayu@gmail.com>
2021-02-16 19:58:31 +0900

    add disabled log in case

--- components/FirebaseAuth.js
+++ components/FirebaseAuth.js
@@ -27,6 +27,10 @@ const firebaseAuthConfig = {
         setUserCookie( userData )
         return
       }
+      if ( firebase.auth().currentUser.disabled ) {
+        // should showing 403 Error "you are not invited"
+        return
+      }
       await firebase.auth().currentUser.sendEmailVerification()
       firebase
         .auth()
