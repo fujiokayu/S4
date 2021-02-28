e9e37c8bf622fdcded84ef80cca051a21e4dc880
fujiokayu <fujiokayu@gmail.com>
2021-02-28 20:03:12 +0900

    modify list layout

--- components/List.tsx
+++ components/List.tsx
@@ -55,16 +55,17 @@ const List = (props) => {
         <div className="siimple-list">
           {files.length > 0 ? (
             files.map((file) => (
-              <div>{file.name}
-              <li key={file.md5Hash}>
-                content type: {file.contentType}
-                <li> size: {Math.round(file.size / 1024 * 10) / 10}kb</li>
+              <ul>
+                {file.name}
+              
+                <li key={file.md5Hash}>content type: {file.contentType}</li>
+                <li>size: {Math.round(file.size / 1024 * 10) / 10}kb</li>
                 <li>last updated: {format(new Date(file.updated), 'yyyy/MM/dd HH:mm:ss')}</li>
                 <Download token={props.user.token} file={file.fullPath}/>
                 <Delete token={props.user.token} file={file.fullPath}/>
                 <hr />
-              </li>
-              </div>
+              
+              </ul>
             ))
           ) : (
             <p>アップロードされたファイルはありません</p>
