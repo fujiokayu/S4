3a1790c550ccfab6c445670180e763b44534e8bf
fujiokayu <fujiokayu@gmail.com>
2021-02-28 15:39:46 +0900

    fix #29
    add file metadata

--- components/List.tsx
+++ components/List.tsx
@@ -9,21 +9,34 @@ import firebase from 'firebase/app'
 initFirebase()
 
 const List = (props) => {
-  const [files, setFiles] = useState(null)
+  const [files, setFiles] = useState([])
   const uid = useContext(uidContext)
+
   useEffect(() => {
     const f = async () => {
-      if (uid) {
-        const listRef = firebase.storage().ref(uid)
-        const result = await listRef.listAll()
-        .catch(function(error) {
-          console.log(error)
-        })
+      if (!uid || files.length > 0) {
+        return 
+      }
+
+      const listRef = firebase.storage().ref(uid)
+      const result = await listRef.listAll()
+      .catch(function(error) {
+        alert('list file error: ' + error)
+      })
   
-        if (result) {
-          console.log(result)
-          setFiles(result.items)
-        }
+      if (result) {
+        let fileList = [...files]
+        result.items.forEach(element => {
+          const fileRef = firebase.storage().ref(uid).child(element.name)
+          fileRef.getMetadata().then(function(metadata) {
+            const item = { name: element.name, contentType: metadata.contentType, size: metadata.size, updated: metadata.updated, md5Hash: metadata.md5Hash }
+            fileList.push(item)
+          }).catch(function(error) {
+            alert('getMetadata error: ' + error)
+          })
+        })
+        console.log('set', fileList)
+        setFiles(fileList)
       }
     }
     f();
@@ -35,8 +48,9 @@ const List = (props) => {
         <div className="siimple-list">
           {files.length > 0 ? (
             files.map((file) => (
-              <li key={file.name}>
-                {file.name}: 
+              <li key={file.md5Hash}>
+                {file.name}: {file.contentType} {Math.round(file.size / 1024 * 10) / 10}kb　<br />
+                last updated: {format(new Date(file.updated), 'yyyy/MM/dd HH:mm:ss')}
                 <Download token={props.user.token} file={file.fullPath}/>
                 <Delete token={props.user.token} file={file.fullPath}/>
                 <hr />
