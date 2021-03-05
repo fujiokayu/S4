c8ae65883438718527ab84eae12be51f9b77a9ba
fujiokayu <fujiokayu@gmail.com>
2021-03-05 20:33:13 +0900

    再レンダリングのためのネストを解除した

--- components/List.tsx
+++ components/List.tsx
@@ -14,40 +14,42 @@ const List = (props) => {
 
   const uid = useContext(uidContext)
   useEffect(() => {
+    setLoaded(false)
     const f = async () => {
       if (!uid) {
         return 
       }
 
-      setLoaded(false)
       const listRef = firebase.storage().ref(uid)
       const result = await listRef.listAll()
       .catch(function(error) {
         alert('list file error: ' + error)
       })
   
-      if (result) {
-        let fileList = [...files]
-        fileList.splice(0)
-        result.items.forEach(element => {
-          const fileRef = firebase.storage().ref(uid).child(element.name)
-          fileRef.getMetadata().then(function(metadata) {
-            const item = { 
-              name: element.name,
-              contentType: metadata.contentType,
-              size: metadata.size,
-              updated: metadata.updated,
-              md5Hash: metadata.md5Hash,
-              fullPath: metadata.fullPath}
-            fileList.push(item)
-          }).catch(function(error) {
-            alert('getMetadata error: ' + error)
-          })
-        })
-        console.log('set', fileList)
-        setFiles(fileList)
-        setLoaded(true)
+      if (!result) {
+        return
       }
+
+      let fileList = [...files]
+      fileList.splice(0)
+      result.items.forEach(element => {
+        const fileRef = firebase.storage().ref(uid).child(element.name)
+        fileRef.getMetadata().then(function(metadata) {
+          const item = { 
+            name: element.name,
+            contentType: metadata.contentType,
+            size: metadata.size,
+            updated: metadata.updated,
+            md5Hash: metadata.md5Hash,
+            fullPath: metadata.fullPath}
+          fileList.push(item)
+        }).catch(function(error) {
+          alert('getMetadata error: ' + error)
+        })
+      })
+      console.log('set', fileList)
+      setFiles(fileList)
+      setLoaded(true)
     }
     f();
   }, [uid])
