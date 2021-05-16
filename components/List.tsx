fab466daf91c68d8f6ebe1da9ea90f87d86ac7c0
fujiokayu <fujiokayu@gmail.com>
2021-05-16 10:30:09 +0900

    add react-table for List component

--- components/List.tsx
+++ components/List.tsx
@@ -1,26 +1,28 @@
-import Download from '../components/Download'
-import Delete from '../components/Delete'
+import DataTable from '../components/DataTable'
 import { uidContext } from '../pages/index';
 import { format, add } from 'date-fns'
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext, useMemo } from 'react';
 import firebase from 'firebase/app'
 import { listAll } from '../utils/storage/storageUtil'
 
 interface IFiles {
 	name: string,
 	contentType: string,
-	size: number,
-	updated: Date,
-	md5Hash: string,
-	fullPath: string,
+	size: string,
+	filePath: string,
+	updated: string,
+	expireDate: string,
   user: string
 }
 
 const List = (props) => {
+  
   const [files, setFiles] = useState<IFiles[]>([])
   const [loaded, setLoaded] = useState(false)
   const [update, setUpdate] = useState(false)
 
+
+    
   //子コンポーネントからの更新要求
   const changeState = (isState) => {
     setUpdate(isState)
@@ -36,7 +38,7 @@ const List = (props) => {
         return 
       }
 
-      const result = await listAll(uid)  
+      const result = await listAll(uid)
       if (!result) {
         return
       }
@@ -48,11 +50,11 @@ const List = (props) => {
           const item: IFiles = {
             name: element.name,
             contentType: metadata.contentType,
-            size: metadata.size,
-            updated: metadata.updated,
-            md5Hash: metadata.md5Hash,
-            fullPath: metadata.fullPath,
-            user: metadata.customMetadata.createUser
+            size: Math.round(metadata.size / 1024 * 10) / 10 + 'kb',
+            updated: format(new Date(metadata.updated), 'yyyy/MM/dd HH:mm'),
+            expireDate: format(add(new Date(metadata.updated), {days: 7}), 'yyyy/MM/dd HH:mm'),
+            user: metadata.customMetadata.createUser,
+            filePath: metadata.fullPath
           }
           newList.push(item)
         }).catch(function(error) {
@@ -78,18 +80,7 @@ const List = (props) => {
   return (
     <div className="siimple-list">
       {files.length > 0 ? (
-        files.map((file) => (
-          <ul>
-            <span>{file.name}</span> : {file.contentType}
-            <li key={file.size}>ファイルサイズ　　　：{Math.round(file.size / 1024 * 10) / 10}kb</li>
-            <li key={file.md5Hash}>ファイル更新日　　　：{format(new Date(file.updated), 'yyyy/MM/dd HH:mm:ss')}</li>
-            <li key={file.fullPath}>有効期限　　　　　　：{format(add(new Date(file.updated), {days: 7}), 'yyyy/MM/dd HH:mm:ss')}</li>
-            <li key={file.user}>アップロードユーザー：{file.user}</li>
-            <Download file={file.fullPath}/>
-            <Delete file={file.fullPath} changeState={changeState}/>
-            <hr />
-          </ul>
-        ))
+        <DataTable data={files} changeState={changeState}/>
       ) : (
         <p>アップロードされたファイルはありません</p>
       )}
