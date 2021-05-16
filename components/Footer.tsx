8f096725ddc03b4d47f36321394764f3ae652423
fujiokayu <fujiokayu@gmail.com>
2021-05-16 10:25:12 +0900

    add Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-example siimple--bg-primary siimple--color-white">
+      Copyright © foobar Inc. All Rights Reserved.
+    </div>
+)
+}
+
+export default Footer
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,21 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import Footer from '../components/Footer'
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
         <Head>
-        <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
+          <header style={{margin: 'auto'}}>
+            <div className='siimple-jumbotron-title' style={{fontSize: 70, color: 'darkgray', margin: '20px'}}>title</div>
+          </header>
           <Main />
           <NextScript />
         </body>
+        <Footer />
       </Html>
     )
   }
